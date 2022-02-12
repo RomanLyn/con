f2a6d81fec8d0ebfb765f2d90b31675e69a634b4
RomanLyn <iromanlyn@gmail.com>
2022-02-12 18:23:05 +0200

    st

--- aa.sql
+++ aa.sql
@@ -1,3 +1,15 @@
-select * 
-from ggg
-where d1=15234
+…or create a new repository on the command line
+
+echo "# con" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/RomanLyn/con.git
+git push -u origin main
+
+…or push an existing repository from the command line
+
+git remote add origin https://github.com/RomanLyn/con.git
+git branch -M main
+git push -u origin main
